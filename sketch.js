632e3d6d5e8f75341de22655c91ecc210dcc058f
devinmidha28 <69163361+devinmidha28@users.noreply.github.com>
2020-09-26 12:48:53 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+var ground1
+
+var block8 
+var block9  
+var block10 
+var block11 
+var block12  
+var block13 
+var block14  
+var block15  
+var block16
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
+
+ground1 = new Ground (100,400)
+
+block8 = new Box 
+block9 = new Box 
+block10 = new Box 
+block11 = new Box 
+block12 = new Box 
+block13 = new Box 
+block14 = new Box 
+block15 = new Box 
+block16 = new Box 
